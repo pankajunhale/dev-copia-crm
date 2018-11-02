2495bf05d6f462cc2e208f74a3a3d81c845cb68d
g0li <iprogramyourapp@gmail.com>
2018-11-02 17:53:11 +0530

    RS: BOM and Routing UI FROZEN :)

--- src/app/routing/routing-edit/routing-edit.component.ts
+++ src/app/routing/routing-edit/routing-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-routing-edit',
